603887c9eb4fb71f4285bf1da5fe97be4c53a299
Christophe Junke <christophe.junke@parrot.com>
2019-09-19 13:56:01 +0200

    [DEV] Add libpython.a symbol link when building python-native

--- atom.mk
+++ atom.mk
@@ -59,19 +59,38 @@ ifneq ("$(PYTHON_NATIVE_BIN)","")
 PYTHON_NATIVE_VERSION := $(strip $(shell $(PYTHON_NATIVE_BIN) -c \
 	"import sys; print('{0.major:d}.{0.minor:d}'.format(sys.version_info))"))
 
+#
 # Include directories, do not get system headers for virtual env
-# Python3 adds an 'm' at the end of the version for include directory
+#
 ifndef CONFIG_PYTHON_NATIVE_USE_VIRTUAL_ENV
   LOCAL_EXPORT_C_INCLUDES += $(shell $(PYTHON_NATIVE_BIN)-config --includes)
 endif
+
+#
+# Python3 adds an 'm' at the end of the version for include directory
+# See also: python3-config --abiflags
+#
 ifdef CONFIG_PYTHON_NATIVE_VERSION_3
-  LOCAL_EXPORT_C_INCLUDES += $(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR)/include/python$(PYTHON_NATIVE_VERSION)m
+  PYTHON_ABI_NAME := python$(PYTHON_NATIVE_VERSION)m
 else
-  LOCAL_EXPORT_C_INCLUDES += $(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR)/include/python$(PYTHON_NATIVE_VERSION)
+  PYTHON_ABI_NAME := python$(PYTHON_NATIVE_VERSION)
 endif
 
+PYTHON_CONFIG_DIR := $(shell $(PYTHON_NATIVE_BIN)-config --configdir)
+PYTHON_STATIC_LIB := $(shell readlink -e $(PYTHON_CONFIG_DIR)/lib$(PYTHON_ABI_NAME).a)
 
+LOCAL_EXPORT_C_INCLUDES += $(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR)/include/$(PYTHON_ABI_NAME)
 LOCAL_EXPORT_LDLIBS := $(shell $(PYTHON_NATIVE_BIN)-config --ldflags)
+
+#
+# Versionless symbol links to actual python locations, that depends on
+# build options. Modules that depends on python-native muse use those
+# links.
+#
+PYTHON_TARGET_LIB := $(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR)/lib/libpython.a
+PYTHON_TARGET_BIN := $(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR)/lib/python
+
+PYTHON_HOST_BIN := $(HOST_OUT_STAGING)/$(HOST_ROOT_DESTDIR)/bin/python
 
 endif
 
@@ -89,11 +108,13 @@ define LOCAL_CMD_BUILD
 			$(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR) \
 			--always-copy \
 			--system-site-packages $(endl) \
-		$(Q) ln -sfr $(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR)/bin/python  $(HOST_OUT_STAGING)/$(HOST_ROOT_DESTDIR)/bin/python \
+		$(Q) ln -sfr $(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR)/bin/python $(PYTHON_HOST_BIN) $(endl)\
+	        $(Q) ln -sf $(TARGET_OUT_STAGING)/$(PYTHON_STATIC_LIB) $(PYTHON_TARGET_LIB) \
 		, \
-		$(Q) ln -sf $(PYTHON_NATIVE_BIN) $(HOST_OUT_STAGING)/$(HOST_ROOT_DESTDIR)/bin/python \
+		$(Q) ln -sf $(PYTHON_NATIVE_BIN) $(PYTHON_HOST_BIN) $(endl) \
+		$(Q) ln -sf $(PYTHON_STATIC_LIB) $(PYTHON_TARGET_LIB) \
 	)
-	$(Q) ln -sf python$(PYTHON_NATIVE_VERSION) $(TARGET_OUT_STAGING)/$(TARGET_ROOT_DESTDIR)/lib/python
+	$(Q) ln -sf python$(PYTHON_NATIVE_VERSION) $(PYTHON_TARGET_BIN)
 endef
 
 include $(BUILD_CUSTOM)
